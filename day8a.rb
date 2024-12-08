895a0cdf75eaa77c8c7ce97fe9c757665c3e506b
Marcus Poller <m@debian>
2024-12-08 23:53:32 +0100

    Bug: Will ein Koordinatenpaar (Array) an einen Hash anhängen, dessen Werte eine Liste aus Koordinatenpaaren darstellen soll

---
+++ day8a.rb
@@ -0,0 +1,123 @@
+# AOC Day8a
+
+# Algorithmischer Trick:
+# Die antinodes finden sich auf:
+#  - Antennenvector der beiden Antennen
+#  - Antenne 1 + Vector
+#  - Antenne 1 - Vector
+#  - Antenne 2 + Vector
+#  - Antenne 2 - Vector
+#  - Antenne 1 ist kein Antinode
+#  - Antenne 2 ist kein Antinode
+
+# Ergebnis muss keine Karte sein, es reicht ein Array von Koordinaten
+# Aus diesem Array können Koordinaten gelöscht weren, die
+#  - <0y
+#  - <0x
+#  - >ymax
+#  - xmax sind
+# Doubletten sind zu entfernen
+# Die Länge des Arrays ist die Puzzlelösung
+
+# Parsen des Inputs
+# Hashmap
+#  map['a'] = [ [1,0], [0,0] ]
+
+# Programmablauf
+#
+# Nach dem Parsen für jeden Buchstaben
+#   danach für jedes koordinatenpaar (wie in Bubblesort mit zwei Indizees)
+#     das Array aus koordinaten durchgehen und die Antinodes berechnen
+# abschließend Antinodes filtilren
+
+class Vec 
+  def initialize( y, x )
+    @y=y
+    @x=x
+  end
+  def plus( vec_b)
+    x = @x + vec_b.x
+    y = @y + vec_b.y
+    return Vec.new( y, x)
+  end
+  def x
+    return @x
+  end
+  def y
+    return @y
+  end
+  def diff( vec_b)
+    dx= @x - vec_b.x
+    dy= @y - vec_b.y
+    return Vec.new( dy, dx)
+  end
+  def genSignal( vec_b) # => Array of Coordinate-Array
+    diff = self.diff( vec_b)
+    a = self.plus( diff)
+    b = self.minus( diff)
+    c = vec_b.plus( diff)
+    d = vec_b.minus( diff)
+    res = Array.new
+    res.push(a.asArray).push(b.asArray).push(c.asArray).push(d.asArray)
+    puts "Debug: before #{res.inspect}"
+    res.delete( self.asArray)
+    res.delete( vec_b.asArray )
+#    plan_b = res - self.asArray - vec_b
+#    puts "Debug: after #{plan_b.inspect}"
+    puts "Debug: after #{res.inspect}"
+    return res
+  end
+  def plus( vec_b )
+    x = @x + vec_b.x
+    y = @y + vec_b.y
+    return Vec.new( y, x)
+  end
+  def minus( vec_b )
+    self.plus( Vec.new( -vec_b.y, -vec_b.x))
+  end
+  def asArray # => [y, x]
+    r = Array.new
+    r.push( @y)
+    r.push( @x)
+  end
+end
+
+class Map
+  def initialize( puzzleinputstring)
+    @miny=0
+    @minx=0
+    lines = puzzleinputstring.split("\n")
+    @maxy=lines.length-1
+    @maxx=lines[0].length-1
+    @letters = Hash.new( Array.new )
+    @letters["a"]=[[0,0]]
+    for yidx in 0..@maxy-1
+      l=lines[yidx].chomp
+      for xidx in 0..@maxx-1
+        puts "Dbg: y=#{yidx},x=#{xidx}, l=#{l[xidx]}"
+        if l[xidx] =~ /[a-zA-Z0-9]/
+          puts "Dbg: RegEx match"
+          @letters[ "foo" ].push ( [0,1] )
+          @letters[ l[xidx] ].push( [yidx, xidx] )
+          puts @letters.inspect
+        end
+      end
+    end
+  end
+  def letters
+    return @letters
+  end
+end
+
+a1=Vec.new( 4, 5)
+a2=Vec.new( 6, 6)
+
+puts a1.inspect
+puts a2.inspect
+
+puts a1.genSignal(a2).inspect
+
+pi = File.read( ARGV[0] )
+puts pi
+m=Map.new( pi)
+puts m.letters.inspect
--- timing.md
+++ timing.md
@@ -5,4 +5,12 @@ day2a 00:05:00 01:11:00 #off-by-one, Verdacht auf Fehler in Puzzleinput
 day2b 00:04:00 00:18:00 #off-by-one, Verdacht auf Fehler in Puzzleinput
 day3a 00:07:30 00:16:30
 day3b 00:14:00 01:01:00 #musste Zeilenumbrüche im Input entfernen, damit die Lösung greift
-day4a          00:30:00
+day4a 00:22:00 00:30:00
+day5a 00:09:00 02:00:00
+day5b 00:32:00
+day6a 00:16:00 00:50:00
+day6b 00:53:00 00:30:00
+day7a 00:06:18
+day7b 00:03:14
+day8a 00:14:00 00:50:00
+day8b 00:05:00
